c8160aba8af45a36e672051c8866ecef799d6fef
Earl Floden <earl.mark.27@gmail.com>
2017-05-17 11:29:07 +1000

    Separate game logic and world building functions for master controller architecture

--- main.js
+++ main.js
@@ -1,34 +1,54 @@
+// Global Variables
 var scale = 64;
-// The Games tile map
-function drawMap() {
-    var map = {
-        cols: 8,
-        rows: 14,
-        tsize: 64,
-        tiles: [
-            4, 34, 34, 34, 34, 34, 34, 5,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            19, 73, 73, 73, 73, 73, 73, 17,
-            20, 2, 2, 2, 2, 2, 2, 21
-        ],
-        getTile: function (col, row) {
-            return this.tiles[row * map.cols + col];
-        }
+var level = 1;
+// Game Maps/Levls
+var map1 = {
+    cols: 8,
+    rows: 14,
+    tsize: 64,
+    tiles: [
+        4, 34, 34, 34, 34, 34, 34, 5,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        19, 73, 73, 73, 73, 73, 73, 17,
+        20, 2, 2, 2, 2, 2, 2, 21
+    ],
+    getTile: function (col, row) {
+        return this.tiles[row * map1.cols + col];
     }
-    // $('#world').css("height", "512px");
-    // $('#world').css("width", "512px");
-    //$('#world').css("background-color","blue");
-    $("#world").append("<div id='player' style='left: "+ 256 +"px; top: "+ 700 +"px; '></div>");
+}
+
+/*
+ * Initializes the game on page load, master controller of the application.
+ */
+$(document).ready(function() {
+    drawMap(map1);
+    drawPlayer(256, 700);
+    drawNavyPatrol(256, 100, 90);
+    $('#fireCannon').onTap(function() {
+        var leftPos = $("#player").position().left;
+        var topPos = $("#player").position().top;
+        var rot = getRotationDegrees($("#player"));
+        rot += 180;
+        rot %= 360;
+        $("#world").append("<div class='cannonBall'></div>");
+        $(".cannonBall").moveTo(rot);
+    });
+});
+
+/*
+ * Draws the world map tiles to the viewport
+ */
+function drawMap(map) {
     for (var c = 0; c < map.cols; c++) {
         for (var r = 0; r < map.rows; r++) {
             var tile = map.getTile(c, r);
@@ -46,26 +66,43 @@ function drawMap() {
             }
         }
     }
-    $("#world").append("<div class='enemyShip' style='left: "+ 256 +"px; top: "+ 100 +"px; '></div>");
-    $(".enemyShip").moveTo(90);
+};
+
+/*
+ * Draws the player to the game world and defines player interactions.
+ */
+function drawPlayer(startX, startY) {
+    $("#world").append("<div id='player' style='left: "+ startX +"px; top: "+ startY +"px; '></div>");
+}
+
+/*
+ * Draws a Navy Patrol boat to the game world and defines its interactions.
+ */
+function drawNavyPatrol(startX, startY, heading) {
+    $("#world").append("<div class='enemyShip' style='left: "+ startX +"px; top: "+ startY +"px; '></div>");
+    $(".enemyShip").moveTo(heading);
     $(".enemyShip").speed(0.1);
     $(".enemyShip").autoBounceOff(true);
     $(".enemyShip").onCollision(function(object) {
-        this.css("transform", "rotate(90deg)");
+        var facing  = getRotationDegrees(this);
+        facing += 90;
+        this.css("transform", "rotate("+facing+"deg)");
     });
+    // On player tap shoot a cannonball towards this boat.
     $(".enemyShip").onTap(function(event) {
       $("#world").append("<div class='cannonBall'></div>");
-      var leftPos = parseInt($(".enemyShip").css("left"), 10);
-      var topPos = parseInt($(".enemyShip").css("top"), 10);
+      var leftPos = $(".enemyShip").position().left;
+      var topPos = $(".enemyShip").position().top;
       $(".cannonBall").moveTo(leftPos, topPos);
     });
-};
+}
+
 /*
  * Moves and orientates the player towards an event (eg: onTap)
  */
 function movePlayerTo(event) {
-    var leftPos = parseInt($("#player").css("left"), 10);
-    var topPos = parseInt($("#player").css("top"), 10);
+    var leftPos = $("#player").position().left;
+    var topPos = $("#player").position().top;
     var angle = calculateAngle(event.clientY, topPos, event.clientX, leftPos);
     // fix our orientation
     angle += 270;
@@ -75,6 +112,7 @@ function movePlayerTo(event) {
     $("#player").moveTo(event.clientX, event.clientY);
     $("#player").speed(0.2);
 }
+
 /*
  * Calculates the angle from element a to element b
  */
@@ -87,20 +125,10 @@ function calculateAngle(aY, bY, aX, bX) {
     }
     return angle;
 }
-$(document).ready(function() {
-    drawMap();
-    var leftPos = parseInt($("#player").css("left"), 10);
-    var topPos = parseInt($("#player").css("top"), 10);
-    $('#fireCannon').onTap(function() {
-        var leftPos = parseInt($("#player").css("left"), 10);
-        var topPos = parseInt($("#player").css("top"), 10);
-        var rot = getRotationDegrees($("#player"));
-        rot += 180;
-        rot %= 360;
-        $("#world").append("<div class='cannonBall'></div>");
-        $(".cannonBall").moveTo(rot);
-    });
-});
+
+/*
+ * Calculates the current rotation of an element (eg a ships heading)
+ */
 function getRotationDegrees(obj) {
     var matrix = obj.css("-webkit-transform") ||
     obj.css("-moz-transform")    ||
